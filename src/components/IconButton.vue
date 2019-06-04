28b9fb458e1cfde805d5d73384952bd7ca6648dd
Adriana Ochoa <suadocca@gmail.com>
2019-06-04 15:08:59 -0600

    IconButton without click event

---
+++ src/components/IconButton.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="buttonContainer" :style="styling" >
+      <img :src="img" />
+    </div>
+</template>
+
+<script>
+//import rubbish from '../assets/icons/rubbish-bin.png'
+
+export default {
+  props:{
+      color: String,
+      img:String
+  },
+  computed: {
+      styling: function(){
+          return{
+              'background-color':this.color
+          }
+      }
+  } 
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+h1, h2 {
+  font-weight: normal;
+}
+
+.buttonContainer{
+  padding: 5px 5px 1px 5px;
+  display: inline-block;
+  border-radius: 5px;
+}
+
+img{
+  width: 25px;
+  height: 24px;
+}
+
+</style>
