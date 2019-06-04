2c9823c1939e708ec43587812637bfbd291dc5d3
VictorNicolasLO <victornicolaslo@hotmail.com>
2019-06-04 00:30:57 -0600

    first commit

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <input type="text" :placeholder="placeholder" :value="value" @input="onInput">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "searchBox",
+  props: {
+    placeholder: String,
+    value: String
+  },
+  methods: {
+    onInput(ev) {
+      this.$emit("input", ev.target.value);
+    }
+  }
+};
+</script>
+<style scope>
+input {
+  padding: 5px;
+}
+</style>
---
+++ src/views/Playground.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    Aqui practica con tus componentes
+    {{exampleData}}
+    <SearchBox v-model="exampleData"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SearchBox from "../components/SearchBox";
+
+export default {
+  name: "playground",
+  data() {
+    return {
+      exampleData: "example"
+    };
+  },
+  components: {
+    SearchBox
+  }
+};
+</script>
