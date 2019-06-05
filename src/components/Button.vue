f4d006bbbdeebfa91b3c38497ebf3762ef26895b
JEGF <kukieloy05@gmail.com>
2019-06-05 10:44:41 -0600

    Componente button primer commit

---
+++ src/components/Button.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="button" :style="styling" @click="onClick($event)" >
+    <div class="texto">
+        {{text}}
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  props: {
+    color: String,
+    text: String
+  },
+  computed:{
+    styling: function(){
+      return{
+        'background-color':this.color
+      }
+    }
+  },
+  methods: {
+    onClick(ev) {
+      this.$emit(ev.target.tagName)
+    }
+  }
+};
+</script>
+<style scope>
+.button{
+
+  width: 92px;
+  height: 38px;
+  border-radius:5px;
+  color: white;
+}
+.texto{
+  padding-top: 9.5px;
+}
+
+</style>
--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -1,24 +1,30 @@
 <template>
   <div class="home">
-    Aqui practica con tus componentes
-    {{exampleData}}
-    <SearchBox v-model="exampleData"/>
+    
+    <Button color="#FC5757" text="Eliminar"  />
+    <br>
+    <Button color="#7ED87C" text="Añadir"  />
+
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import SearchBox from "../components/SearchBox";
+// import SearchBox from "../components/SearchBox";
+import Button from "../components/Button";
 
 export default {
   name: "playground",
   data() {
     return {
-      exampleData: "example"
-    };
+
+     };
+
   },
   components: {
-    SearchBox
+    // SearchBox,
+    Button
   }
 };
 </script>
