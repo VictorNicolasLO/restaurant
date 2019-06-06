8e1a6850942faafc51de2d60ceb0ddaa30b34a61
roussita17 <rosita_gv17@hotmail.com>
2019-06-06 10:22:06 -0600

    Componente Terminado con Funcionalidad Disabled

---
+++ src/components/SmallInputs.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+    <input class="SInput" type="number" min="0" :value="value" :disabled="disabled" @input="onInput"/> 
+    </div>
+</template>
+
+<script>
+export default {
+    name:"SmallInputs", 
+    props:{
+        value:Number,
+        disabled:Boolean
+    },
+    methods: {
+    onInput(e) {
+      this.$emit("input", e.target.value);
+    }
+  }
+};
+</script>
+
+
+<style scope >
+.SInput{
+     position: absolute;
+     width: 42px;
+     height:15px;
+     margin-top:5px;
+     border: 1px solid gray;
+     border-radius: 5px;
+     font-family: 'Roboto';
+     font-style: normal;
+     font-weight: normal;
+     font-size: 16px;
+     line-height: 19px;
+     text-align: center;
+
+     color: #000000;
+}
+
+    
+</style>
--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -2,23 +2,28 @@
   <div class="home">
     <TextField  :placeholder="placeholder" :title="title"/>
     <TextField  placeholder="a" title="b"/>
+    <SmallInputs text="text" :disabled="false" :value="value" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import TextField from "../components/TextField";
+import SmallInputs from "../components/SmallInputs";
 export default {
   name: "playground",
   data() {
     return {
       exampleData: "example",
       title: "Descripción de la Mesa:",
-      placeholder:"Descripcion de la Mesa"
+      placeholder:"Descripcion de la Mesa",
+      value:0
+
     }
   },
   components: {
-    TextField
+    TextField,
+    SmallInputs
    // textito
   }
 }
